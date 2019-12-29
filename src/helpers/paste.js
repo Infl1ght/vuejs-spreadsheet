141f3f1160d49ef0bab79ace3685a919d9fd68af
Ilya Volkov <ilya.inflight@gmail.com>
2019-12-29 23:57:43 +0300

    Removed console.log

--- src/helpers/paste.js
+++ src/helpers/paste.js
@@ -5,7 +5,5 @@ export default function divideTextDataToCells(textData) {
   const textDataFiltered = textData.replace(/\r/g, '');
   const rowsArray = textDataFiltered.split('\n');
   const cellsArray = rowsArray.map(row => row.split('\t'));
-  console.log(textDataFiltered);
-  console.log(cellsArray[1][0]);
   return cellsArray;
 }
